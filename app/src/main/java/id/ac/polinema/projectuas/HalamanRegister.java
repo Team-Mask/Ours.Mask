d306352e9aed61ae73ba8e8776942b6bc03296e9
febtrianiari <febtria.niari@gmail.com>
2019-12-30 00:16:49 +0700

    Login, Register , Masker 2

--- app/src/main/java/id/ac/polinema/projectuas/HalamanRegister.java
+++ app/src/main/java/id/ac/polinema/projectuas/HalamanRegister.java
@@ -2,7 +2,9 @@ package id.ac.polinema.projectuas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HalamanRegister extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class HalamanRegister extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_register);
+    }
+
+    public void handlerRegister(View view) {
+        Intent intent = new Intent(this, HalamanUtama.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/id/ac/polinema/projectuas/models/masker.java
@@ -0,0 +1,5 @@
+package id.ac.polinema.projectuas.models;
+
+public class masker {
+
+}
